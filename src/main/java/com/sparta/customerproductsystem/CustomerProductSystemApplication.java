2f2d8c6bd5101341aa4af714b35b4b9bd340296e
leo <dhwjdqls0409@naver.com>
2025-10-31 11:40:53 +0900

    Application 추가

---
+++ src/main/java/com/sparta/customerproductsystem/CustomerProductSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.customerproductsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProductSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerProductSystemApplication.class, args);
+    }
+
+}
