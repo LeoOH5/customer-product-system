b2db1cebeea4c440803db607760e70a31db627a1
B-ChJ <bchaejoo@gmail.com>
2025-11-03 15:43:10 +0900

    Security 적용 전 변경사항 저장

--- src/main/java/com/sparta/customerproductsystem/controller/OrderController.java
+++ src/main/java/com/sparta/customerproductsystem/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.sparta.customerproductsystem.controller;
 
 import com.sparta.customerproductsystem.domain.entity.Users;
+import com.sparta.customerproductsystem.dto.orderdto.DeleteOrderResponse;
 import com.sparta.customerproductsystem.dto.orderdto.OrderRequest;
 import com.sparta.customerproductsystem.dto.orderdto.OrderResponse;
 import com.sparta.customerproductsystem.service.OrderService;
@@ -9,8 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +26,11 @@ public class OrderController {
     public ResponseEntity<OrderResponse> getDetailOrders(@PathVariable Long orderId,
                                                                @AuthenticationPrincipal Users user) {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.getOne(orderId, user));
+    }
+
+    @DeleteMapping("/orders/{orderId}")
+    public ResponseEntity<DeleteOrderResponse> deleteOrder(@PathVariable Long orderId,
+                                                           @AuthenticationPrincipal Users user) {
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.delete(orderId, user));
     }
 }
--- src/main/java/com/sparta/customerproductsystem/service/OrderService.java
+++ src/main/java/com/sparta/customerproductsystem/service/OrderService.java
@@ -3,6 +3,9 @@ package com.sparta.customerproductsystem.service;
 import com.sparta.customerproductsystem.domain.entity.Order;
 import com.sparta.customerproductsystem.domain.entity.Product;
 import com.sparta.customerproductsystem.domain.entity.Users;
+import com.sparta.customerproductsystem.domain.role.OrderRole;
+import com.sparta.customerproductsystem.domain.role.UserRole;
+import com.sparta.customerproductsystem.dto.orderdto.DeleteOrderResponse;
 import com.sparta.customerproductsystem.dto.orderdto.OrderRequest;
 import com.sparta.customerproductsystem.dto.orderdto.OrderResponse;
 import com.sparta.customerproductsystem.repository.OrderRepository;
@@ -31,11 +34,10 @@ public class OrderService {
         if(request.getQuantity() > product.getStock()) {
             throw new IllegalStateException("재고가 부족합니다. 구매 수량을 수정해주세요.");
         }
-        String status = "OK";
-
         int amount = product.getPrice() * request.getQuantity();
 
-        Order order = new Order(user, product, request.getQuantity(), amount, status);
+        Order order = new Order(user, product, request.getQuantity(), amount);
+        order.setStatus(OrderRole.OK);
         Order savedOrder = orderRepository.save(order);
 
         return OrderResponse.from(savedOrder);
@@ -48,9 +50,28 @@ public class OrderService {
                 () -> new IllegalStateException("요청 파라미터가 유효하지 않습니다."));
 
         // 관리자 조회 시 전체 사용자의 Order 조회 가능
-        if(user.getRole().equals("ADMIN")) {
+        if(user.getRole().equals(UserRole.ADMIN)) {
             return OrderResponse.from(order);
         }
+        // 사용자 조회 시 해당 사용자 Order 건만 조회 가능
+        // 사용자 일치하는지 check
+        if(!user.getId().equals(order.getUser().getId())) {
+            throw new IllegalArgumentException("해당 요청은 관리자만 수행할 수 있습니다.");
+        }
+        return OrderResponse.from(order);
+    }
 
+    @Transactional
+    public DeleteOrderResponse delete(Long orderId, Users user) {
+        Order order = orderRepository.findById(orderId).orElseThrow(
+                () -> new IllegalStateException("요청 파라미터가 유효하지 않습니다."));
+
+        // 삭제를 요청한 사용자가 Order user_id와 같은지 check
+        if(!user.getId().equals(order.getUser().getId())) {
+            throw new IllegalArgumentException("삭제 권한이 없습니다.");
+        }
+        Long id = order.getId();
+        orderRepository.delete(order);
+        return new DeleteOrderResponse(id, OrderRole.CANCEL);
     }
 }
