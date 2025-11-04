e1ee879db1c465913a95d0642dc37fb05f1bb3d5
sjww0306 <sjww03@gmail.com>
2025-11-04 22:40:58 +0900

    refactor : 코드 컨벤션 준수를 위한 일부 수정
    - JwtUtils : JWT_SECRET_KEY 변수명 .env 내 선언부분으로 일치화
    
    - AuthService : UserDetailRepository 삭제로 인하여 변경된 레포지토리로 수정
    
    - Users : 코드 컨벤션 준수

--- src/main/java/com/sparta/customerproductsystem/service/AuthService.java
+++ src/main/java/com/sparta/customerproductsystem/service/AuthService.java
@@ -7,7 +7,7 @@ import com.sparta.customerproductsystem.dto.LoginResponse;
 import com.sparta.customerproductsystem.dto.SignUpRequest;
 import com.sparta.customerproductsystem.dto.SignUpResponse;
 import com.sparta.customerproductsystem.jwt.JwtUtils;
-import com.sparta.customerproductsystem.repository.UsersDetailRepository;
+import com.sparta.customerproductsystem.repository.UserRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,25 +16,25 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    private final UsersDetailRepository usersDetailRepository;
+    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
 
     public SignUpResponse saveUsers(@Valid SignUpRequest request) {
-        if (usersDetailRepository.existsByEmail(request.getEmail())) {
+        if (userRepository.existsByEmail(request.getEmail())) {
             throw new IllegalArgumentException("존재하는 이메일입니다");
         }
         Users users = new Users(request.getEmail(),
                 passwordEncoder.encode(request.getPassword()),
                 request.getName(),
                 UserRole.CUSTOMER);
-        usersDetailRepository.save(users);
+        userRepository.save(users);
 
         return new SignUpResponse(users);
     }
 
     public LoginResponse login(LoginRequest loginRequest) {
-        Users users = usersDetailRepository.findByEmail(loginRequest.getEmail())
+        Users users = userRepository.findByEmail(loginRequest.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다"));
 
         if (!passwordEncoder.matches(loginRequest.getPassword(), users.getPassword())) {
