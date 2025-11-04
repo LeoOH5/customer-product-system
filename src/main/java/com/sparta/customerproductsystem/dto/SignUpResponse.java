45822ce9991f93e351b6692520c47772ebf3df8b
tlsrltjq <psl87531@naver.com>
2025-11-04 10:17:47 +0900

    feat: 유저 로그인 기능 구현, JWT 토큰 생성

--- src/main/java/com/sparta/customerproductsystem/dto/SignUpResponse.java
+++ src/main/java/com/sparta/customerproductsystem/dto/SignUpResponse.java
@@ -10,14 +10,12 @@ public class SignUpResponse {
     private Long id;
     private String email;
     private String name;
-    private String phone;
     private LocalDateTime createAt;
 
     public SignUpResponse(Users user) {
         this.id = user.getId();
         this.email = user.getEmail();
         this.name =  user.getName();
-        this.phone = user.getPhone();
         this.createAt = user.getCreatedAt();
     }
 }
