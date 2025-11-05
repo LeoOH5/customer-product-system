75427d844bd5b0ec348700ce4302315047507de9
LeoOH5 <bbin0409@naver.com>
2025-11-05 21:02:05 +0900

    fix : 예외기능 제거

--- src/main/java/com/sparta/customerproductsystem/service/ProductService.java
+++ src/main/java/com/sparta/customerproductsystem/service/ProductService.java
@@ -95,9 +95,6 @@ public class ProductService {
             product.setPrice(patchProductRequest.getPrice());
         }
         if (patchProductRequest.getStockQuantity() != null) {
-            if(patchProductRequest.getStockQuantity() < 0){
-                BusinessException.of(ErrorCode.INVALID_PRODUCT_UPDATE);
-            }
             product.setStockQuantity(patchProductRequest.getStockQuantity());
         }
 
